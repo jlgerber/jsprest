44b6831237cfb83b2b43a5ffddd95e26fd4317ac
jgerber <jlgerber@gmail.com>
2019-08-11 00:01:13 -0700

    out with the old

--- src/routes/shows.rs
+++
@@ -1,8 +0,0 @@
-use crate::{get_showlist, ShowList};
-use actix_web::Responder;
-
-pub fn index() -> impl Responder {
-    let result = get_showlist();
-    // HttpResponse::Ok().body(&ShowList::new(result))
-    ShowList::new(result)
-}
